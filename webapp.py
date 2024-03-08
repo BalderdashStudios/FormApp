20104422c829c3d0b8f92e2232bff6c25a22056a
BalderdashStudios <h.ray.emerald@gmail.com>
2024-03-08 13:59:30 -0800

    Added iMac G3 Year number selector

--- webapp.py
+++ webapp.py
@@ -18,5 +18,9 @@ def render_page2():
 def render_page3():
     return render_template('page3.html')
     
+@app.route("/temp")
+def render_temp():
+    return render_template('RepairsForms/YearNumber.html')
+    
 if __name__=="__main__":
     app.run(debug=False)
