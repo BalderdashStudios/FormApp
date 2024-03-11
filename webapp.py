a946cbbde4fb7747b18eca2e8fa21858dc5cce53
BalderdashStudios <h.ray.emerald@gmail.com>
2024-03-11 14:34:43 -0700

    Added python form integration

--- webapp.py
+++ webapp.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for, render_template
+from flask import Flask, url_for, render_template, request
 
 app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
 
@@ -21,6 +21,25 @@ def render_page3():
 @app.route("/temp")
 def render_temp():
     return render_template('RepairsForms/YearNumber.html')
+
+@app.route("/yearNumber")
+def render_yearNumber():
+    proccesor = request.args['proccesor']
+    
+    return render_template('RepairsForms/YearNumber.html')
+
+
+
     
 if __name__=="__main__":
     app.run(debug=False)
+
+
+#Dictionary
+
+my_dictionary = {
+    "car" : "an automobile",
+    "fact": "something that actually exists; reality, truth"
+}
+
+#First is key, second is value. print(md["the"]) |  md["dog"] = md["dog"]+1 | if "hi" id md keys
