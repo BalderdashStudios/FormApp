1e1c212385253a40c79319549c62008b84482254
BalderdashStudios <h.ray.emerald@gmail.com>
2024-03-18 14:33:45 -0700

    Added CSS borders and removed gray scale

--- static/script.js
+++ static/script.js
@@ -2,7 +2,6 @@ $(document).ready(function() {
 	$(".ImgSel").click(function() { 
 		$('.ImgSel').removeClass('ImgSelChoice');
 		$(this).addClass('ImgSelChoice');
-		$(this).css("filter", "grayscale(100%)");
 		$(this).css('border-width', '5px'); 
 	});
 })
